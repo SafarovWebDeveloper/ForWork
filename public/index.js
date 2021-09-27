5a4c3ca8eb4650411e88dfad9634898ad1a76ba6
SafarovWebDeveloper <odilovismoil0@gmail.com>
2021-09-27 15:12:06 -0500

    beta version start

--- public/index.js
+++ public/index.js
@@ -2,11 +2,6 @@ const currentLocation = location.href;
 const menuItems = document.querySelectorAll('.navigation li a');
 const menuLength = menuItems.length;
 
-loginBtn.addEventListener('click', ()=>{
-    formRegister.style.display = 'none';
-    alert(1);
-})
-
 for(let i = 0; i<menuLength; i++){
     if(menuItems[i].href === currentLocation){
         menuItems[i].className = 'active'
